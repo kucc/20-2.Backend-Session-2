19b5d57232281e7644fd86040eaeabcb9a208613
easymean <m2162003@gmail.com>
2020-10-02 01:02:50 +0900

    feat: make model by DB schema

---
+++ 이지민/demo/src/main/java/com/todo/demo/model/Category.java
@@ -0,0 +1,38 @@
+package com.todo.demo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString
+@Table(name="category")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Setter
+    @Column(name="title", length=100)
+    private String title;
+
+    @Setter
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<ToDo> toDos = new ArrayList<ToDo>();
+
+    public void addToDo(ToDo t){
+        List<ToDo> toDos = getToDos();
+        toDos.add(t);
+    }
+
+    @Builder
+    public Category(String title){
+        this.title = title;
+    }
+}
---
+++ 이지민/demo/src/main/java/com/todo/demo/model/ToDo.java
@@ -0,0 +1,53 @@
+package com.todo.demo.model;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name="todo")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Setter
+    @Column(name="content")
+    @Type(type="text")
+    private String content;
+
+    @Setter
+    @Column(name="finished", columnDefinition = "TINYINT")
+    private Boolean finished;
+
+    @Column(name="created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Setter
+    @Column(name="finished_at")
+    @CreationTimestamp
+    private LocalDateTime finishedAt;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    @Builder
+    public ToDo(String content, Boolean finished, LocalDateTime createdAt, LocalDateTime finishedAt, Category category){
+        this.content = content;
+        this.finished = finished;
+        this.createdAt = createdAt;
+        this.finishedAt = finishedAt;
+        this.category = category;
+    }
+
+
+}
