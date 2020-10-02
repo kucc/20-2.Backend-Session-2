320c0f328938f33245c64db5b07ec22a5d854780
easymean <m2162003@gmail.com>
2020-10-02 17:03:13 +0900

    feat: make user entity implement UserDetail for using JAVA serutiry

--- 이지민/demo/src/main/java/com/todo/demo/model/User.java
+++ 이지민/demo/src/main/java/com/todo/demo/model/User.java
@@ -11,6 +11,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.hibernate.annotations.Type;
+import org.modelmapper.internal.util.Assert;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
@@ -18,13 +26,13 @@ import org.hibernate.annotations.Type;
 @ToString(exclude="password")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name="user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private Long id;
 
-    @Column(name="email", nullable = false, length=320)
+    @Column(name="email", unique = true, nullable = false, length=320)
     private String email;
 
     @Setter
@@ -41,11 +49,45 @@ public class User {
     @Type(type="text")
     private String profile_image;
 
+    private String auth;
+
     @Builder
     public User(String email, String password, String username){
+        Assert.notNull(email, "email must not be null");
+        Assert.notNull(password, "password must not be null");
+        Assert.notNull(username, "username must not be null");
+
         this.email = email;
         this.password = password;
         this.username = username;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> roles = new HashSet<>();
+        for(String role:auth.split(",")){
+            roles.add(new SimpleGrantedAuthority(role));
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
