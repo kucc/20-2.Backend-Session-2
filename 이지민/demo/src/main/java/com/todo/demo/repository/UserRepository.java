1a50590c66408e21f858830c4b87a50b00a7ab54
easymean <m2162003@gmail.com>
2020-10-02 17:07:03 +0900

    feat: make  JPQL using interface

--- 이지민/demo/src/main/java/com/todo/demo/repository/UserRepository.java
+++ 이지민/demo/src/main/java/com/todo/demo/repository/UserRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.todo.demo.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+    Optional<User> findByUsername(String name);
 }
