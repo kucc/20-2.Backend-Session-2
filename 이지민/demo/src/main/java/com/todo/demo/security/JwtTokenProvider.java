1a94ffddc6b15758e3de4c1908cb40cd840f5409
easymean <m2162003@gmail.com>
2020-10-08 11:50:50 +0900

    feat: make jwt token provider

---
+++ 이지민/demo/src/main/java/com/todo/demo/security/JwtTokenProvider.java
@@ -0,0 +1,77 @@
+package com.todo.demo.security;
+
+
+import com.todo.demo.model.User;
+import com.todo.demo.service.AuthService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${SECRET_KEY}")
+    private String secretKey;
+
+    private final long tokenValidTime = 30 * 60 * 1000L;
+
+    private final AuthService authService;
+
+    @Autowired
+    public JwtTokenProvider(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostConstruct
+    protected void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String userPk){
+        Claims claims = Jwts.claims().setSubject(userPk);
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token){
+        User user = authService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+    }
+
+    // 토큰에서 회원 정보 추출
+    public String getUserPk(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // request의 헤더에서 token을 가져온다.
+    public String resolveToken(HttpServletRequest request){
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    //토큰의 유효성 확인
+    public boolean validateToken(String jwtToken){
+        try{
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e){
+            return false;
+        }
+    }
+}
