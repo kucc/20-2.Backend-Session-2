275c5f3eae624d7d378ff688fe04d0464a5f432b
easymean <m2162003@gmail.com>
2020-10-08 11:58:55 +0900

    feat: make login and sign up api

--- 이지민/demo/src/main/java/com/todo/demo/repository/UserRepository.java
+++ 이지민/demo/src/main/java/com/todo/demo/repository/UserRepository.java
@@ -6,8 +6,8 @@ import com.todo.demo.model.User;
 
 import java.util.Optional;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
-
-    Optional<User> findByUsername(String name);
+    Optional<User> findById(Long id);
 }
--- 이지민/demo/src/main/java/com/todo/demo/service/AuthService.java
+++ 이지민/demo/src/main/java/com/todo/demo/service/AuthService.java
@@ -1,41 +1,55 @@
 package com.todo.demo.service;
 
 import com.todo.demo.dto.user.UserDto;
+import com.todo.demo.exception.CustomUserNotFoundException;
 import com.todo.demo.model.User;
 import com.todo.demo.repository.UserRepository;
+import com.todo.demo.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public User createUser(UserDto.Request requestDto){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        requestDto.setPassword(encoder.encode(requestDto.getPassword()));
-        User user = User.builder()
-                .email(requestDto.getEmail())
-                .password(requestDto.getPassword())
-                .username(requestDto.getUsername()).build();
-       userRepository.save(user);
-       return user;
+        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        User user = requestDto.toEntity();
+        userRepository.save(user);
+        return user;
     }
 
-    public Boolean emailCheck(String email){
+    public Boolean emailDoesNotExist(String email){
         return userRepository.findByEmail(email) == null;
     }
 
+    public Boolean passwordMatch(String password, String email){
+        User user = userRepository.findByEmail(email);
+        return passwordEncoder.matches(password, user.getPassword());
+    }
+
+    public String createTokenById(String email){
+        User user = userRepository.findByEmail(email);
+        return jwtTokenProvider.createToken(String.valueOf(user.getId()));
+    }
+
     @Override
-    public User loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException((username)));
+    public User loadUserByUsername(String userPK) throws UsernameNotFoundException {
+        return userRepository.findById(Long.valueOf(userPK))
+                .orElseThrow(CustomUserNotFoundException::new);
+
     }
 }
