f46b17f3e3a241cf941cd3ba22eaa371e594c1f1
easymean <m2162003@gmail.com>
2020-10-08 16:17:53 +0900

    fix: fix unresolvable circular reference

--- 이지민/demo/src/main/java/com/todo/demo/service/AuthService.java
+++ 이지민/demo/src/main/java/com/todo/demo/service/AuthService.java
@@ -1,49 +1,19 @@
 package com.todo.demo.service;
 
-import com.todo.demo.dto.user.UserDto;
 import com.todo.demo.exception.CustomUserNotFoundException;
 import com.todo.demo.model.User;
 import com.todo.demo.repository.UserRepository;
-import com.todo.demo.security.JwtTokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService implements UserDetailsService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    @Autowired
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
+    public AuthService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    public User createUser(UserDto.Request requestDto){
-        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
-        User user = requestDto.toEntity();
-        userRepository.save(user);
-        return user;
-    }
-
-    public Boolean emailDoesNotExist(String email){
-        return userRepository.findByEmail(email) == null;
-    }
-
-    public Boolean passwordMatch(String password, String email){
-        User user = userRepository.findByEmail(email);
-        return passwordEncoder.matches(password, user.getPassword());
-    }
-
-    public String createTokenById(String email){
-        User user = userRepository.findByEmail(email);
-        return jwtTokenProvider.createToken(String.valueOf(user.getId()));
     }
 
     @Override
--- 이지민/demo/src/main/java/com/todo/demo/service/UserService.java
+++ 이지민/demo/src/main/java/com/todo/demo/service/UserService.java
@@ -3,8 +3,8 @@ package com.todo.demo.service;
 import com.todo.demo.dto.user.UserDto;
 import com.todo.demo.model.User;
 import com.todo.demo.repository.UserRepository;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.todo.demo.security.JwtTokenProvider;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +13,13 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
 
@@ -29,6 +32,27 @@ public class UserService {
         User user = userRepository.getOne(id);
 
         return user;
+    }
+
+    public User createUser(UserDto.Request requestDto){
+        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        User user = requestDto.toEntity();
+        userRepository.save(user);
+        return user;
+    }
+
+    public Boolean emailDoesNotExist(String email){
+        return userRepository.findByEmail(email) == null;
+    }
+
+    public Boolean passwordMatch(String password, String email){
+        User user = userRepository.findByEmail(email);
+        return passwordEncoder.matches(password, user.getPassword());
+    }
+
+    public String createTokenById(String email){
+        User user = userRepository.findByEmail(email);
+        return jwtTokenProvider.createToken(String.valueOf(user.getId()));
     }
 
 }
