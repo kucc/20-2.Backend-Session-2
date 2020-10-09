127f68b59b67a585bc0c8f47f9997086d9c497dc
easymean <m2162003@gmail.com>
2020-10-09 22:08:07 +0900

    feat: add request body annotation

--- 이지민/demo/src/main/java/com/todo/demo/controller/AuthController.java
+++ 이지민/demo/src/main/java/com/todo/demo/controller/AuthController.java
@@ -4,11 +4,14 @@ import com.todo.demo.dto.user.UserDto;
 import com.todo.demo.exception.CustomUserNotFoundException;
 import com.todo.demo.model.User;
 import com.todo.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -20,7 +23,7 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<UserDto.Response> signUp(UserDto.Request requestDto) throws Exception{
+    public ResponseEntity<UserDto.Response> signUp(@RequestBody UserDto.Request requestDto) throws Exception{
 
         if(!userService.emailDoesNotExist(requestDto.getEmail())){
             throw new Exception("이미 존재하는 이메일입니다.");
@@ -30,12 +33,13 @@ public class AuthController {
             throw new Exception("비밀번호를 확인해주세요.");
         }
 
-        User user = userService.createUser(requestDto);
+        int userId = userService.createUser(requestDto);
+        User user = userService.findOne(userId);
         return ResponseEntity.ok().body(new UserDto.Response(user));
     }
 
     @PostMapping("/login")
-    public  ResponseEntity<String> login(UserDto.Login loginDto){
+    public  ResponseEntity<String> login(@RequestBody UserDto.Login loginDto){
 
         if(userService.emailDoesNotExist(loginDto.getEmail())){
             throw new CustomUserNotFoundException("가입되지 않은 email 입니다.");
